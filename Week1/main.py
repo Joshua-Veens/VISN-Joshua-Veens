0bac64a1453fdef4bedbbfd0a97f6287a1750a69
Joshua-Veens <78480354+Joshua-Veens@users.noreply.github.com>
2022-02-16 21:18:10 +0100

    Hue added

--- Week1/main.py
+++ Week1/main.py
@@ -3,10 +3,14 @@ from skimage import io
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import matplotlib.colors as cl
 
 image = plt.imread("image.jpg")
 
 imageArray = np.array(image)
+
+def toHSV(arrayToSet):
+    return cl.rgb_to_hsv(arrayToSet)
 
 def changeColor(array, color, colorRangeLower, colorRangeUpper):
     for y in range(array.shape[0]):
@@ -17,14 +21,24 @@ def changeColor(array, color, colorRangeLower, colorRangeUpper):
                     break
     return array
 
-
 brick = (220,55,45)
 grass = (9,95,8)
+colors = []
 
-plt.figure(0)
-plt.plot(imgplot = plt.imshow(imageArray))
+def getHUE(array, colors):
+    tmpColors = []
+    for y in range(array.shape[0]):
+        for x in range(array.shape[1]):
+            colors.append(array[y, x, 0]*360)
+    return colors
+
+
 plt.figure(1)
 plt.plot(imgplot = plt.imshow(changeColor(imageArray.copy(), brick, 170, 25)))
 plt.figure(2)
 plt.plot(imgplot = plt.imshow(changeColor(imageArray.copy(), grass, 80, 25)))
+plt.figure(3)
+plt.hist((getHUE(toHSV(imageArray.copy()),colors)))
+plt.figure(4)
+plt.hist(getHUE(toHSV(changeColor(imageArray.copy(), brick, 170, 25)),colors))
 plt.show()
