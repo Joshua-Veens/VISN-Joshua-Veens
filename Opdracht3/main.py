8cbaf7faceb21caa7839b379a9cf16b33c08bebf
Joshua-Veens <78480354+Joshua-Veens@users.noreply.github.com>
2022-03-08 15:06:33 +0100

    Opdracht 5 af

---
+++ Opdracht3/main.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import ndimage as ndi
+from skimage.util import random_noise
+from skimage import data, feature, filters
---
+++ Opdracht5/main.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn import svm
+import random
+
+digits = datasets.load_digits()
+trainingData = []
+trainingTarget = []
+
+testImages = []
+testData = []
+testTarget = []
+
+def sortData():
+    for digit in range(len(digits.data)):
+        randNr = random.randint(1,3)
+        if randNr > 2:
+            testData.append(digits.data[digit])
+            testTarget.append(digits.target[digit])
+            testImages.append(digits.images[digit])
+        if randNr <= 2:
+            trainingData.append(digits.data[digit])
+            trainingTarget.append(digits.target[digit])
+
+sortData()
+
+print(len(trainingData))
+print(len(testData))
+
+clf = svm.SVC(gamma=0.001, C=100)
+X,y = trainingData, trainingTarget
+clf.fit(X,y)
+
+print(clf.predict(testData[-4:-3]))
+plt.imshow(testImages[-4], cmap=plt.cm.gray_r, interpolation='nearest')
+
+plt.show()
